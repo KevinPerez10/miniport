6e64757593628c3ed69a0c6f6b9447eb09e6e29e
KevinPerez10 <66193315+KevinPerez10@users.noreply.github.com>
2022-08-23 07:26:11 +0800

    Add files via upload

---
+++ components/Interests.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Interests() {
+    return (
+        <div className="interests">
+            <h1 className="interests--title"> Interests </h1>
+            <h2 className="interests--content">
+                Cyclist. Drummer. Photographer. Programmer. Anime Lover
+            </h2>
+        </div>
+    )
+}
